029355c13358eced775d184f37d6a23b6758a9cf
Rajan Prakash Chand <rajanprakash.chand08@gmail.com>
2025-03-14 19:42:41 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    host: '0.0.0.0', // Expose server on all interfaces
+    port: 8080, // Default port
   },
+  base: '/me.rajanchandthakuri/',  // Specify the base URL for GitHub Pages
   plugins: [
+    // React plugin for React
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // Conditionally load componentTagger for development mode
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'), // Alias for src directory
     },
   },
 }));
