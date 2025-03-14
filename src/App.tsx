10d81f3b21da4a646a6e057c8790bee13ab42f49
Rajan Prakash Chand <rajanprakash.chand08@gmail.com>
2025-03-14 19:16:06 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
